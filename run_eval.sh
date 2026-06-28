370d868731a626d7e043bfb500d1a52b82ee7dd1
Guaaaava <641331276@qq.com>
2026-06-28 17:42:39 +0800

    clean branch: code only, remove generated files

--- run_eval.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/huangcy/Graph-Learning-BP/GNN_learning
-/home/huangcy/anaconda3/envs/py310/bin/python -u evaluate.py > /tmp/eval_full.log 2>&1
-echo "DONE" >> /tmp/eval_full.log
